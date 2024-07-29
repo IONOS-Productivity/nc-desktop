a1d822146d92e4b1a3e9de4902f1a6fa3d95d74a
Emil Bohleber <emil.bohleber@brickmakers.de>
2024-07-29 13:22:34 +0200

    [SES-42] try autoclicker approach

---
+++ .github/workflows/autobuild.ps1
@@ -0,0 +1,24 @@
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+Start-Process powershell.exe -ArgumentList "-file ${env:HOME}\.github\workflows\runcraft.ps1" 
+
+Start-Sleep -m 3000
+
+[System.Windows.Forms.SendKeys]::SendWait("${env:HOME}\Craft64")
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+
+Start-Sleep -m 20000
+
+[System.Windows.Forms.SendKeys]::SendWait("0")
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+
+Start-Sleep -m 1000
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+
+Start-Sleep -m 1000
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+
+Start-Sleep -m 1000
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+
+Start-Sleep -m 1000
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
---
+++ .github/workflows/runcraft.ps1
@@ -0,0 +1,1 @@
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/KDE/craft/master/setup/install_craft.ps1'))
