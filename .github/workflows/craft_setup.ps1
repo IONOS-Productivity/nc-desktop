d4bd7ca26dc3162d8e14555da4d3eeb14a45d619
Emil Bohleber <emil.bohleber@brickmakers.de>
2024-07-29 09:42:44 +0200

    [SES-42] remove workspace

--- .github/workflows/craft_setup.ps1
+++ .github/workflows/craft_setup.ps1
@@ -9,8 +9,9 @@ $RepoRoot = "{0}\..\.." -f ([System.IO.Path]::GetDirectoryName($myInvocation.MyC
 $command = @("${env:HOME}\craft\CraftMaster\CraftMaster\CraftMaster.py",
              "--config", "${RepoRoot}\craftmaster.ini",
             #  "--config-override", "${RepoRoot}/.github/workflows/craft_override.ini",
-             "--target", "${env:CRAFT_TARGET}",
-             "--variables", "WORKSPACE=${env:HOME}\craft") + $args
+             "--target", "${env:CRAFT_TARGET}") + $args
+            #  "--variables", "WORKSPACE=${env:HOME}\craft")
+            #   + $args
 
 Write-Host "Exec: ${python} ${command}"
 
