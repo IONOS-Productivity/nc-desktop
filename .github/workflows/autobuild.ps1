70b29668cfbf4ff2d025444c7aafb8d101f15023
Emil Bohleber <emil.bohleber@brickmakers.de>
2024-07-29 15:06:46 +0200

    [SES-42] add debug prints

--- .github/workflows/autobuild.ps1
+++ .github/workflows/autobuild.ps1
@@ -8,17 +8,14 @@ Start-Sleep -m 3000
 [System.Windows.Forms.SendKeys]::SendWait("${env:HOME}\Craft64")
 [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
 
-Start-Sleep -m 1000
-[System.Windows.Forms.SendKeys]::SendWait("Y")
-[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+Write-Output "Entered Root Path"
 
 Start-Sleep -m 20000
 
 [System.Windows.Forms.SendKeys]::SendWait("0")
 [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
 
-Start-Sleep -m 1000
-[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+Write-Output "Chose QT version"
 
 Start-Sleep -m 1000
 [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
@@ -28,5 +25,10 @@ Start-Sleep -m 1000
 
 Start-Sleep -m 1000
 [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+
+Start-Sleep -m 1000
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+
+Write-Output "Install all"
 
 $proc.WaitForExit()
