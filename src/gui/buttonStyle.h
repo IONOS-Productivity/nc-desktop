321c39dc97fb66c06c3aa9a7d22e175d26919fd0
Christopher Hätty <christopher.haetty@brickmakers.de>
2024-08-06 12:08:55 +0200

    [SES-27][SES-66] Added new buttonStyle classes

---
+++ src/gui/buttonStyle.h
@@ -0,0 +1,214 @@
+
+#ifndef _BUTTONSTYLE_H
+#define _BUTTONSTYLE_H
+
+#include <QString>
+#include "ionostheme.h"
+#include "sesButton.h"
+
+namespace OCC{
+
+enum class ButtonStyleName {
+    Primary,
+    Secondary,
+};
+
+class ButtonStyle 
+{
+protected:
+    ButtonStyle()
+    {
+    }
+    ~ButtonStyle() {}
+
+public:
+
+    // Default
+    virtual QString buttonDefaultColor() const = 0;
+    virtual QString buttonDefaultBorderColor() const = 0;
+    // Hover
+    virtual QString buttonHoverColor() const = 0;
+    virtual QString buttonHoverBorderColor() const = 0;
+    // Pressed
+    virtual QString buttonPressedColor() const = 0;
+    virtual QString buttonPressedBorderColor() const = 0;
+    // Disabled
+    virtual QString buttonDisabledColor() const = 0;
+    virtual QString buttonDisabledBorderColor() const = 0;
+    // Focused
+    virtual QString buttonFocusedColor() const = 0;
+    virtual QString buttonFocusedBorderColor() const = 0;
+    // Font
+    virtual QString buttonDisabledFontColor() const = 0;
+    virtual QString buttonFontColor() const = 0;
+};
+
+class PrimaryButtonStyle : public ButtonStyle {
+private: 
+    PrimaryButtonStyle()
+    {
+    }
+    ~PrimaryButtonStyle() {}
+public:
+
+    PrimaryButtonStyle(PrimaryButtonStyle &other) = delete;
+    void operator=(const PrimaryButtonStyle &) = delete;
+
+    static PrimaryButtonStyle& GetInstance() {
+        static PrimaryButtonStyle instance;
+        return instance;
+    }
+
+    // Default
+    QString buttonDefaultColor() const override 
+    {
+        return OCC::IonosTheme::buttonPrimaryColor();
+    }
+
+    QString buttonDefaultBorderColor() const override 
+    {
+        return OCC::IonosTheme::buttonPrimaryColor();
+    }
+
+    //Hover
+    QString buttonHoverColor() const override 
+    {
+        return OCC::IonosTheme::buttonPrimaryHoverColor();
+    }
+
+    QString buttonHoverBorderColor() const override 
+    {
+        return OCC::IonosTheme::buttonPrimaryHoverColor();
+    }
+
+    // Pressed
+    QString buttonPressedColor() const override 
+    {
+        return OCC::IonosTheme::buttonPrimaryPressedColor();
+    }
+
+    QString buttonPressedBorderColor() const override 
+    {
+        return OCC::IonosTheme::buttonPrimaryPressedColor();
+    }
+
+    // Disabled
+    QString buttonDisabledColor() const override 
+    {
+        return OCC::IonosTheme::buttonDisabledColor();
+    }
+
+    QString buttonDisabledBorderColor() const override 
+    {
+        return OCC::IonosTheme::buttonDisabledColor();
+    }
+
+    // Focused
+    QString buttonFocusedColor() const override 
+    {
+        return OCC::IonosTheme::buttonPrimaryColor();
+    }
+
+    QString buttonFocusedBorderColor() const override 
+    {
+        return OCC::IonosTheme::black();
+    }
+
+    // Font
+    QString buttonDisabledFontColor() const override 
+    {
+        return OCC::IonosTheme::buttonDisabledFontColor();
+    }
+
+    QString buttonFontColor() const override 
+    {
+        return OCC::IonosTheme::white();
+    }
+};
+
+class SecondaryButtonStyle : public ButtonStyle {
+protected: 
+    SecondaryButtonStyle()
+    {
+    }
+    ~SecondaryButtonStyle() {}
+public:
+
+    SecondaryButtonStyle(SecondaryButtonStyle &other) = delete;
+    void operator=(const SecondaryButtonStyle &) = delete;
+
+    static SecondaryButtonStyle& GetInstance() {
+        static SecondaryButtonStyle instance; 
+        return instance;
+    }
+
+    // Default
+    QString buttonDefaultColor() const override 
+    {
+        return OCC::IonosTheme::white();
+    }
+
+    QString buttonDefaultBorderColor() const override 
+    {
+        return OCC::IonosTheme::buttonSecondaryBorderColor();
+    }
+
+    // Hover
+    QString buttonHoverColor() const override 
+    {
+        return OCC::IonosTheme::buttonSecondaryHoverColor();
+    }
+
+    QString buttonHoverBorderColor() const override 
+    {
+        return OCC::IonosTheme::buttonSecondaryBorderColor();
+    }
+
+    // Pressed
+    QString buttonPressedColor() const override 
+    {
+        return OCC::IonosTheme::buttonSecondaryPressedColor();
+    }
+
+    QString buttonPressedBorderColor() const override 
+    {
+        return OCC::IonosTheme::buttonSecondaryBorderColor();
+    }
+
+    // Disabled
+    QString buttonDisabledColor() const override 
+    {
+        return OCC::IonosTheme::buttonDisabledColor();
+    }
+
+    QString buttonDisabledBorderColor() const override 
+    {
+        return OCC::IonosTheme::buttonDisabledColor();
+    }
+
+    // Focused 
+    QString buttonFocusedColor() const override 
+    {
+        return OCC::IonosTheme::white();
+    }
+
+    QString buttonFocusedBorderColor() const override 
+    { 
+        return OCC::IonosTheme::black();
+    }
+
+    // Font
+    QString buttonDisabledFontColor() const override 
+    {
+        return OCC::IonosTheme::buttonDisabledFontColor();
+    }
+
+    QString buttonFontColor() const override 
+    {
+        return OCC::IonosTheme::black();
+    }
+};
+
+
+}
+#endif // _BUTTONSTYLE_H
