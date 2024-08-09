a86157c3ca84398e58d1cb6009cfbf8e5bae14b5
Christopher Hätty <christopher.haetty@brickmakers.de>
2024-08-09 14:41:41 +0200

    [SES-27][SES-69] Implemented Widget for (Error) Snackbar

---
+++ src/gui/sesSnackBar.cpp
@@ -0,0 +1,86 @@
+#include "sesSnackBar.h"
+#include "IonosTheme.h"
+#include "Theme.h"
+#include <QLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+
+namespace OCC {
+
+    sesSnackBar::sesSnackBar(QWidget* parent)
+        : QFrame(parent)
+    {
+        setObjectName("sesSnackBar");
+        setMinimumHeight(40);
+        setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+        setContentsMargins(0, 0, 0, 0);
+
+        auto policy = sizePolicy();
+        policy.setRetainSizeWhenHidden(true);
+        setSizePolicy(policy);
+        
+        const auto layout = new QHBoxLayout();
+        layout->setObjectName("sesSnackBarLayout");
+        layout->setContentsMargins(0, 0, 0, 0);
+        layout->setSpacing(0);
+
+        m_captionLabel.setObjectName("sesSnackBarCaption");
+        m_captionLabel.setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Minimum);
+        m_captionLabel.setText(m_caption);
+
+        m_messageLabel.setObjectName("sesSnackBarMessage");
+        m_messageLabel.setText(m_message);
+        m_messageLabel.setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Minimum);
+        m_messageLabel.setWordWrap(true);
+
+        const auto iconLabel = new QLabel();
+        iconLabel->setObjectName("sesSnackBarIcon");
+        iconLabel->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        iconLabel->setFixedSize(16, 16);
+        const auto logoIconFileName = Theme::hidpiFileName(":/client/theme/ses/ses-snackBarErrorIcon.svg");
+        iconLabel->setPixmap(logoIconFileName);
+
+        layout->addSpacerItem(new QSpacerItem(16, 0, QSizePolicy::Fixed, QSizePolicy::Fixed));
+        layout->addWidget(&m_captionLabel);
+        layout->addSpacerItem(new QSpacerItem(8, 0, QSizePolicy::Fixed, QSizePolicy::Fixed));
+        layout->addWidget(&m_messageLabel);
+        layout->addSpacerItem(new QSpacerItem(8, 0, QSizePolicy::Fixed, QSizePolicy::Fixed));
+        layout->addWidget(iconLabel);
+        layout->addSpacerItem(new QSpacerItem(16, 0, QSizePolicy::MinimumExpanding, QSizePolicy::Fixed));
+        setLayout(layout);
+
+        QString style = QLatin1String("QFrame {border: 1px solid #EEACB2; border-radius: 4px;"
+                                      "background-color: #FDF3F4; color: #000000;}"
+                                      "QLabel {border: 0px none; padding 0px; background-color: transparent; color: #000000;}"
+                                      "QLabel#sesSnackBarCaption {font-weight: bold;}"
+                                      );
+
+
+
+        setStyleSheet(style);
+    }
+
+    QString sesSnackBar::caption() const { return m_caption; }
+    QString sesSnackBar::message() const { return m_message; }
+
+    
+    void sesSnackBar::setCaption(QString captionText) {
+        if (m_caption != captionText) {
+            m_caption = captionText;
+            m_captionLabel.setText(m_caption);
+            emit captionChanged(m_caption);
+        }
+    }
+
+    void sesSnackBar::setMessage(QString messageText) {
+        if (m_message != messageText) {
+            m_message = messageText;
+            m_messageLabel.setText(m_message);
+            emit messageChanged(m_message);
+        }
+    }
+
+    void sesSnackBar::updateStyleSheet()
+    {
+    }
+}
---
+++ src/gui/sesSnackBar.h
@@ -0,0 +1,43 @@
+#ifndef SESSNACKBAR_H
+#define SESSNACKBAR_H
+
+#include <QPushButton>
+#include <QLabel>
+#include <QWidget>
+#include <QMouseEvent>
+#include <QPainter>
+#include <QStylePainter>
+#include <QStyleOptionButton>
+
+namespace OCC {
+
+    class sesSnackBar : public QFrame
+    {
+        Q_OBJECT
+        Q_PROPERTY(QString caption READ caption WRITE setCaption NOTIFY captionChanged)
+        Q_PROPERTY(QString message READ message WRITE setMessage NOTIFY messageChanged)
+
+    public:
+        explicit sesSnackBar(QWidget* parent = nullptr);
+        QString caption() const;
+        QString message() const;
+       
+    public slots:
+        void setCaption(QString captionText);
+        void setMessage(QString messageText);
+    
+    signals:
+        void captionChanged(QString captionText);
+        void messageChanged(QString messageText);
+
+    private:
+        QString m_caption;
+        QString m_message;
+
+        QLabel m_messageLabel;
+        QLabel m_captionLabel;
+
+        void updateStyleSheet();
+    };
+}
+#endif // SESSNACKBAR_H
