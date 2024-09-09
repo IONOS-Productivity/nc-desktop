8f19ae1b7a1399bc32caea0d122cada84abb733e
Christopher Hätty <christopher.haetty@brickmakers.de>
2024-09-09 12:05:30 +0200

    [SES-43][WIP] Added PushButtonStyleHelper Helper Methods
    The icon color will be modifed using a custom painert in tintPixmap

--- src/gui/pushButtonStyleHelper.h
+++ src/gui/pushButtonStyleHelper.h
@@ -17,16 +17,24 @@ class QPainter;
 class QPushButton;
 class QStyleOptionButton;
 class QWidget;
+class QPixmap;
+class QColor;
 
 class PushButtonStyleHelper
 {
 public:
+    void setupPainterForToolButtonShape(const QStyleOptionButton *option, QPainter *painter, const QWidget *widget);
     void setupPainterForShape(const QStyleOptionButton *option, QPainter *painter, const QWidget *widget);
     void drawButtonShape(const QStyleOptionButton *option, QPainter *painter, const QWidget *widget);
+    void drawToolButtonShape(const QStyleOptionButton *option, QPainter *painter, const QWidget *widget);
     QSize sizeFromContents(const QStyleOptionButton *option, QSize contentsSize, const QWidget *widget, int margin) const;
     void adjustTextPalette(QStyleOptionButton *option, const QWidget *widget) const;
     bool isPrimary(const QWidget *widget, const QStyleOptionButton *option) const;
+    void adjustIconColor(QStyleOptionButton *option, const QWidget *widget);
+
 private:
     void recalculateContentSize(QSize &contentsSize, const QWidget *widget) const;
+    QPixmap tintPixmap(const QPixmap &src, const QColor &color) const;
+
 };
 #endif // PUSHBUTTONSTYLEHELPER_H
