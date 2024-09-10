9cc953f53822b2c615bbfc4c77471e0fb505e90e
Christopher Hätty <christopher.haetty@brickmakers.de>
2024-09-10 13:47:29 +0200

    [SES-43] Extracted ButtonStyleStrategy

--- src/gui/pushButtonStyleHelper.cpp
+++ src/gui/pushButtonStyleHelper.cpp
@@ -12,6 +12,7 @@
 
  
 #include "buttonstyle.h"
+#include "buttonStyleStrategy.h"
 #include "ionostheme.h"
 #include <QMap>
 #include <QPainter>
@@ -19,54 +20,6 @@
 #include <QPushButton>
 #include <QWidget> 
 
-class ButtonStyleStrategy
-{
-public:
-    virtual ~ButtonStyleStrategy() = default;
-
-    static OCC::ButtonStyle& getButtonStyle(const QWidget *widget, const QStyleOptionButton *option)
-    {
-        OCC::ButtonStyleName buttonStyleName;
-        if(widget != nullptr)
-        {
-            buttonStyleName = determineButtonStyleName(widget, option);
-        }
-        else
-        {
-            buttonStyleName = OCC::ButtonStyleName::Secondary;
-        }
-        
-        switch (buttonStyleName)
-        {
-            case OCC::ButtonStyleName::Primary:
-                return OCC::PrimaryButtonStyle::GetInstance();
-            case OCC::ButtonStyleName::Secondary:
-            default:
-                return OCC::SecondaryButtonStyle::GetInstance();
-        }
-    }
-
-    static OCC::ButtonStyleName determineButtonStyleName(const QWidget *widget, const QStyleOptionButton *option)
-    {
-        QVariant propertyValue = widget->property("buttonStyle");        
-        if(propertyValue.isValid()){             
-
-            return propertyValue.value<OCC::ButtonStyleName>();
-        }
-
-        return getButtonStyleNameByObjectName(widget);
-    }
-
-    static OCC::ButtonStyleName getButtonStyleNameByObjectName(const QWidget *widget)
-    {
-        static const QMap<QString, OCC::ButtonStyleName> buttonStyleMap = {
-            {"qt_wizard_finish", OCC::ButtonStyleName::Primary}
-        };
-
-        QString buttonName = widget->objectName();
-        return buttonStyleMap.value(buttonName, OCC::ButtonStyleName::Secondary);
-    }
-};
 
 void PushButtonStyleHelper::setupPainterForToolButtonShape(const QStyleOptionButton *option, QPainter *painter, const QWidget *widget)
 {
