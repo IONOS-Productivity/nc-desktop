e5f65133af5e2cd7b418450f9fb3827694483365
Christopher Hätty <christopher.haetty@brickmakers.de>
2024-10-07 15:19:50 +0200

    [SES-8] Added License Informations for 3rd Party Tracking Code

--- src/gui/ga4/ganalytics_worker.cpp
+++ src/gui/ga4/ganalytics_worker.cpp
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Nextcloud Destop - Easystorage
+ *
+ * Modifications:
+ * - Changed the usage of Measurement Protocol to the GA4 API
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "ganalytics.h"
 #include "ganalytics_worker.h"
 #include <QCoreApplication>
@@ -206,6 +224,7 @@ void GAnalyticsWorker::postMessage()
 
     char message[512];
     snprintf(message, sizeof(message), "%s\n", requestUrl.toString().toStdString().c_str());
+    OutputDebugStringA(message);
     logMessage(GAnalytics::Debug, message);
 
     QNetworkReply *reply = networkManager->post(m_request, QByteArray());
