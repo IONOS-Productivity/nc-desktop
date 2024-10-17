a95db83f3c5e26b487e5c016cff5957f15efbe2b
Emil Bohleber <emil.bohleber@brickmakers.de>
2024-10-17 18:18:46 +0200

    [SES-178] strip comments

--- src/gui/SesComponents/syncdirvalidation.cpp
+++ src/gui/SesComponents/syncdirvalidation.cpp
@@ -4,11 +4,9 @@
 #include "logger.h"
 
 #ifdef Q_OS_WIN
-// Implementation of the validate method
 bool SyncDirValidator::isValidDir() {
-    // Add actual validation logic here
       QString appDataPath = SyncDirValidator::appDataPath();
-      return !_path.startsWith(appDataPath) && !appDataPath.startsWith(_path); // Return true if validation is successful, false otherwise
+      return !_path.startsWith(appDataPath) && !appDataPath.startsWith(_path);
 
 }
 
@@ -16,7 +14,6 @@ QString SyncDirValidator::message() {
     return QObject::tr("The directory %1 cannot be part of your sync directory. Please choose another folder.").arg(_path);
 }
 
-// Implementation of the appDataPath method
 QString SyncDirValidator::appDataPath() {
   //Path: AppData/Roaming/<ApplicationName>
   QString appDataRoamingApplicationNamePath = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
@@ -37,7 +34,6 @@ QString SyncDirValidator::message() {
     return "";
 }
 
-// Implementation of the appDataPath method
 QString SyncDirValidator::appDataPath() {
   return "";
 }
--- src/gui/SesComponents/syncdirvalidation.h
+++ src/gui/SesComponents/syncdirvalidation.h
@@ -5,18 +5,15 @@
 
 class SyncDirValidator {
 public:
-    // Constructor that takes a string path
     SyncDirValidator(const QString &path) : _path(path) {}
 
-    // Method to validate the path and users
     bool isValidDir();
     QString message();
 
 private:
-    // Add any private methods or variables here
     QString appDataPath();
-    QString _path; // Member variable to store the path
-};;
+    QString _path;
+};
 
 
 #endif // SYNCDIRVALIDATION_H
