5cbb2a32f64a75ea02fc427b2667a0ffa678976f
Christopher Hätty <christopher.haetty@brickmakers.de>
2025-01-22 15:24:07 +0100

    style settings - systray, main & owncloudgui
    4c098926161b45c8144aae31d048b03234e85d88

--- src/gui/systray.cpp
+++ src/gui/systray.cpp
@@ -15,6 +15,7 @@
 #include "accountmanager.h"
 #include "systray.h"
 #include "theme.h"
+#include "ionostheme.h"
 #include "config.h"
 #include "common/utility.h"
 #include "tray/svgimageprovider.h"
@@ -208,6 +209,12 @@ void Systray::setupContextMenu()
         resumeAction->setVisible(anyPaused);
         resumeAction->setEnabled(anyPaused);
     });
+
+    _contextMenu->setStyleSheet(IonosTheme::fontConfigurationCss(
+        IonosTheme::settingsFont(),
+        IonosTheme::settingsTextSize(),
+        IonosTheme::settingsTextWeight(),
+        IonosTheme::menuTextColor()));
 }
 
 void Systray::destroyDialog(QQuickWindow *dialog) const
