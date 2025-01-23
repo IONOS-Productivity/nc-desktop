96f56cfd1b30d086d859f04bf8c9d605cb363cb4
Christopher Hätty <christopher.haetty@brickmakers.de>
2025-01-23 11:52:00 +0100

    add stylings in libsync
    0c7ae74e69be0e4c2caba5831e229c6f9eec31ec

--- src/libsync/configfile.cpp
+++ src/libsync/configfile.cpp
@@ -997,13 +997,13 @@ void ConfigFile::setNewBigFolderSizeLimit(bool isChecked, qint64 mbytes)
 
 bool ConfigFile::confirmExternalStorage() const
 {
-    const auto fallback = getValue(confirmExternalStorageC, QString(), true);
+    const auto fallback = getValue(confirmExternalStorageC, QString(), false);
     return getPolicySetting(QLatin1String(confirmExternalStorageC), fallback).toBool();
 }
 
 bool ConfigFile::useNewBigFolderSizeLimit() const
 {
-    const auto fallback = getValue(useNewBigFolderSizeLimitC, QString(), true);
+    const auto fallback = getValue(useNewBigFolderSizeLimitC, QString(), false);
     return getPolicySetting(QLatin1String(useNewBigFolderSizeLimitC), fallback).toBool();
 }
 
@@ -1062,7 +1062,7 @@ bool ConfigFile::showMainDialogAsNormalWindow() const {
 bool ConfigFile::promptDeleteFiles() const
 {
     QSettings settings(configFile(), QSettings::IniFormat);
-    return settings.value(QLatin1String(promptDeleteC), false).toBool();
+    return settings.value(QLatin1String(promptDeleteC), true).toBool();
 }
 
 void ConfigFile::setPromptDeleteFiles(bool promptDeleteFiles)
