d73a485460de2963e88bf13a6d064813b933dc79
Kevin Beraz <kevin.beraz@brickmakers.de>
2025-01-24 14:48:46 +0100

    SES-282 remove External Storage checkbox from Setup Page

--- src/gui/wizard/owncloudadvancedsetuppage.cpp
+++ src/gui/wizard/owncloudadvancedsetuppage.cpp
@@ -109,7 +109,7 @@ OwncloudAdvancedSetupPage::OwncloudAdvancedSetupPage(OwncloudWizard *wizard)
     _ui.lServerIcon->setPixmap(appIcon.pixmap(appIconSize));
 
     if (theme->wizardHideExternalStorageConfirmationCheckbox()) {
-        _ui.confCheckBoxExternal->hide();
+        //_ui.confCheckBoxExternal->hide();
     }
     if (theme->wizardHideFolderSizeLimitCheckbox()) {
         _ui.confCheckBoxSize->hide();
@@ -217,7 +217,7 @@ void OwncloudAdvancedSetupPage::initializePage()
     auto newFolderLimit = cfgFile.newBigFolderSizeLimit();
     _ui.confCheckBoxSize->setChecked(newFolderLimit.first);
     _ui.confSpinBox->setValue(newFolderLimit.second);
-    _ui.confCheckBoxExternal->setChecked(cfgFile.confirmExternalStorage());
+    //_ui.confCheckBoxExternal->setChecked(cfgFile.confirmExternalStorage());
 
     SetAvatarIcon();
     setUserInformation();
@@ -459,7 +459,7 @@ bool OwncloudAdvancedSetupPage::validatePage()
             ConfigFile cfgFile;
             cfgFile.setNewBigFolderSizeLimit(_ui.confCheckBoxSize->isChecked(),
                 _ui.confCheckBoxSize->isChecked() ? _ui.confSpinBox->value() : -1);
-            cfgFile.setConfirmExternalStorage(_ui.confCheckBoxExternal->isChecked());
+            //cfgFile.setConfirmExternalStorage(_ui.confCheckBoxExternal->isChecked());
         }
         else
         {
@@ -544,12 +544,12 @@ void OwncloudAdvancedSetupPage::slotSelectiveSyncClicked()
         if (result == QDialog::Accepted) {
             _selectiveSyncBlacklist = dlg->createBlackList();
             updateBlacklist = true;
-            _ui.confCheckBoxExternal->setStyleSheet(IonosTheme::fontConfigurationCss(
-                IonosTheme::settingsFont(),
-                IonosTheme::settingsTextSize(),
-                IonosTheme::settingsTextWeight(),
-                IonosTheme::loginWizardFontLightGrey()
-            ));
+            // _ui.confCheckBoxExternal->setStyleSheet(IonosTheme::fontConfigurationCss(
+            //     IonosTheme::settingsFont(),
+            //     IonosTheme::settingsTextSize(),
+            //     IonosTheme::settingsTextWeight(),
+            //     IonosTheme::loginWizardFontLightGrey()
+            // ));
 
             _ui.confCheckBoxSize->setDisabled(true);
         } else if (result == QDialog::Rejected && _selectiveSyncBlacklist == QStringList("/")) {
@@ -593,12 +593,12 @@ void OwncloudAdvancedSetupPage::slotVirtualFileSyncClicked()
         });
     }
 
-    _ui.confCheckBoxExternal->setStyleSheet(IonosTheme::fontConfigurationCss(
-                IonosTheme::settingsFont(),
-                IonosTheme::settingsTextSize(),
-                IonosTheme::settingsTextWeight(),
-                IonosTheme::loginWizardFontLightGrey()
-            ));
+    // _ui.confCheckBoxExternal->setStyleSheet(IonosTheme::fontConfigurationCss(
+    //             IonosTheme::settingsFont(),
+    //             IonosTheme::settingsTextSize(),
+    //             IonosTheme::settingsTextWeight(),
+    //             IonosTheme::loginWizardFontLightGrey()
+    //         ));
 
     _ui.confCheckBoxSize->setDisabled(true);
 }
@@ -612,12 +612,12 @@ void OwncloudAdvancedSetupPage::slotSyncEverythingClicked()
     QString errorStr = checkLocalSpace(_rSize);
     setErrorString(errorStr);
 
-    _ui.confCheckBoxExternal->setStyleSheet(IonosTheme::fontConfigurationCss(
-        IonosTheme::settingsFont(),
-        IonosTheme::settingsTextSize(),
-        IonosTheme::settingsTextWeight(),
-        IonosTheme::titleColor()
-    ));
+    // _ui.confCheckBoxExternal->setStyleSheet(IonosTheme::fontConfigurationCss(
+    //     IonosTheme::settingsFont(),
+    //     IonosTheme::settingsTextSize(),
+    //     IonosTheme::settingsTextWeight(),
+    //     IonosTheme::titleColor()
+    // ));
 }
 
 
@@ -675,7 +675,7 @@ void OwncloudAdvancedSetupPage::customizeStyle()
     _ui.mainHBox->setContentsMargins(0, 0, 0, 0);
     _ui.wSyncStrategySynchronizeEverything->setContentsMargins(0, 0, 0, 0);
     _ui.lVirtualFileSync->setContentsMargins(0, 0, 0, 0);
-    _ui.horizontalLayout_8->setContentsMargins(32, 0, 0, 0);
+    //_ui.horizontalLayout_8->setContentsMargins(32, 0, 0, 0);
     _ui.horizontalLayout_10->setContentsMargins(0, 8, 0, 0);
 
     _ui.wSyncStrategy->setSpacing(16);
