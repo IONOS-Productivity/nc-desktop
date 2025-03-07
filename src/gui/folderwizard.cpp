657eab78c1a91e74060101d924d1c2690fae7005
Emil Bohleber <emil.bohleber@brickmakers.de>
2025-03-07 12:48:04 +0100

    SES-286 some more translations modifications

--- src/gui/folderwizard.cpp
+++ src/gui/folderwizard.cpp
@@ -70,7 +70,7 @@ QString FormatWarningsWizardPage::formatWarnings(const QStringList &warnings) co
         formattedWarning = QString("%1").arg(warnings.first());
     } else if (warnings.count() > 1) {
         formattedWarning = " <ul>";
-        Q_FOREACH (QString warning, warnings) {
+        for (const auto &warning : warnings) {
             formattedWarning += QString::fromLatin1("<li>%1</li>").arg(warning);
         }
         formattedWarning += "</ul>";
