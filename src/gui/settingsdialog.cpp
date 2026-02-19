60e90e9f271757d7d04e98b7ac326fac546a41f2
Christopher Hätty <christopher.haetty@brickmakers.de>
2026-02-19 11:28:19 +0100

    SES-450 - increased settingsdialog size even more

--- src/gui/settingsdialog.cpp
+++ src/gui/settingsdialog.cpp
@@ -193,8 +193,10 @@ SettingsDialog::SettingsDialog(ownCloudGui *gui, QWidget *parent)
 
     setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
     cfg.restoreGeometry(this);
-    resize(width() > WLTheme.minimalSettingsDialogWidth() + 10 ? width(): WLTheme.minimalSettingsDialogWidth() + 10, 
-        (height() > generalSettings->sizeHint().height() + 50 ? height(): generalSettings->sizeHint().height()) + 50);
+    resize(width() > WLTheme.minimalSettingsDialogWidth() + 50 ? width(): WLTheme.minimalSettingsDialogWidth() + 50, 
+        (height() > generalSettings->sizeHint().height() + 75 ? height(): generalSettings->sizeHint().height()) + 75);
+    setMinimumSize(WLTheme.minimalSettingsDialogWidth() + 50, generalSettings->sizeHint().height() + 75);
+
 }
 
 SettingsDialog::~SettingsDialog()
